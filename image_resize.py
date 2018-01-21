159c0681837fcf9e7cbafe15f90bc74922094760
Sergey Morozov <sergeymorozov@MacBook-Pro-Sergey.local>
2018-01-22 00:37:12 +0300

    использован unpacking

--- image_resize.py
+++ image_resize.py
@@ -9,63 +9,52 @@ def open_image(path_to_original):
     return image
 
 
-def resize_image(image, new_width, new_height):
-    old_width, old_height = image.size[0], image.size[1]
-    if new_width and new_height:
-        new_width = int(new_width)
-        new_height = int(new_height)
-        if old_width/old_height != new_width/new_height:
-            print_message("Proportion won't be saved!")
-        output_image = image.resize(
-            (new_width, new_height),
-            Image.ANTIALIAS
-        )
-        return output_image
-    elif new_width and not new_height:
-        new_width = int(new_width)
+def calculate_new_image_size(old_width, old_height, new_width, new_height):
+    if new_width and not new_height:
         new_height = int(new_width * old_height / old_width)
-        output_image = image.resize(
-            (new_width, new_height),
-            Image.ANTIALIAS
-        )
-        return output_image
+        return new_width, new_height
     elif not new_width and new_height:
-        new_height = int(new_height)
         new_width = int(new_height * old_width / old_height)
-        output_image = image.resize(
-            (new_width, new_height),
-            Image.ANTIALIAS
-        )
-        return output_image
+        return new_width, new_height
     else:
-        output_image = image
-        return output_image
+        check_proportions(new_height, new_width, old_height, old_width)
+        return new_width, new_height
 
 
-def print_message(message):
-    print(message)
+def check_proportions(old_width, old_height, new_width, new_height):
+    if old_width / old_height != new_width / new_height:
+        print("Proportion won't be saved!")
 
 
-def scale_image(image, scale):
+def resize_image(image, new_image_size):
     output_image = image.resize(
-        tuple([int(float(scale) * x) for x in image.size]),
+        new_image_size,
         Image.ANTIALIAS
     )
     return output_image
 
 
-def get_path_to_result(path_to_original, image):
-    file_name = os.path.splitext(path_to_original)[0]
+def scale_image(image, scale):
+    output_image = image.resize(
+        tuple([int(scale * x) for x in image.size]),
+        Image.ANTIALIAS
+    )
+    return output_image
+
+
+def get_path_to_result(path_to_original, image_size):
+    file_name, _ = os.path.splitext(path_to_original)
+    width, height = image_size
     path_to_result = "{}__{}x{}.png".format(
         file_name,
-        image.size[0],
-        image.size[1]
+        width,
+        height
     )
     return path_to_result
 
 
 def save_image(path_to_result, image):
-    image.save(path_to_result, "PNG")
+    image.save(path_to_result)
 
 
 def get_input_argument_parser():
@@ -79,25 +68,24 @@ def get_input_argument_parser():
     parser.add_argument(
         "-o",
         "--output",
-        required=False,
         help="Path to save image"
     )
     parser.add_argument(
         "-w",
         "--width",
-        required=False,
+        type=int,
         help="result image width"
     )
     parser.add_argument(
         "-t",
         "--height",
-        required=False,
+        type=int,
         help="result image height"
     )
     parser.add_argument(
         "-s",
         "--scale",
-        required=False,
+        type=float,
         help=""
 
     )
@@ -109,18 +97,25 @@ if __name__ == "__main__":
     args = parser.parse_args()
     path_to_original = args.file
     path_to_result = args.output
-    width = args.width
-    height = args.height
+    new_width = args.width
+    new_height = args.height
     scale = args.scale
 
-    if scale and (height or width):
+    if scale and (new_width or new_height):
         sys.exit("Wrong parameters. "
                  "Can't be scale and width/height the same time!")
 
     image = open_image(path_to_original)
+    old_width, old_height = image.size
 
-    if height or width:
-        output_image = resize_image(image, width, height)
+    if new_width or new_height:
+        new_image_size = calculate_new_image_size(
+            old_width,
+            old_height,
+            new_width,
+            new_height
+        )
+        output_image = resize_image(image, new_image_size)
     elif scale:
         output_image = scale_image(image, scale)
     else:
@@ -131,6 +126,6 @@ if __name__ == "__main__":
     if path_to_result is None:
         path_to_result = get_path_to_result(
             path_to_original,
-            output_image
+            output_image.size
         )
     save_image(path_to_result, output_image)
